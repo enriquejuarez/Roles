b4bc749d682f140d5a37890ca589ec9480325c98
enriquejuarez <caenjuji@gmail.com>
2018-09-21 00:25:52 -0500

    Add Shinobi, Seeders and Factory. User autentication

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -59,5 +59,7 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'role' => \Caffeinated\Shinobi\Middlewared\UserhasRole::class,
+        'permission' => \Caffeinated\Shinobi\Middlewared\UserhasPermission::class,
     ];
 }
--- config/app.php
+++ config/app.php
@@ -159,6 +159,10 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
+        Caffeinated\Shinobi\ShinobiServiceProvider::class,
+
+
 
     ],
 
@@ -208,6 +212,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+
 
     ],
 
